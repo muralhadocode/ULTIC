662cd61ebbae8c1c2902b9930ec141398d3ef288
mukssd <aquele.eletricista.la@gmail.com>
2026-07-10 17:45:17 -0300

    Change unit value input type from int to float for metric conversion

--- code.py
+++ code.py
@@ -36,7 +36,7 @@ time_units_ons = {
 if slct == 1:
     unit_slct = input("Type the unit you want to convert: ")
     unit = metric_compriment_onm.get(unit_slct)
-    unit_value = int(input("Type the unit value: "))
+    unit_value = float(input("Type the unit value: "))
     unit_slct2 = input("Type the unit of destiny: ")
     unit_dest = metric_compriment_onm.get(unit_slct2)
     unit_in_standard = unit_value*unit
